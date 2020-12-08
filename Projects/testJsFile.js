552f658b1292cd2878440fe1500817b2e415af17
Gihank2040 <60838006+Gihank2040@users.noreply.github.com>
2020-12-08 21:58:59 +0530

    initial commit

---
+++ Projects/testJsFile.js
@@ -0,0 +1,13 @@
+document.write("<h1>Test Header</h1>");
+var num=10;
+var name="gihan";
+var boolien=true;
+var x=20;
+
+var vote=(num>5) ? "old":"young";
+var type=typeof(name);
+while (i < 10) {
+    text += "<br>The number is " + i;
+    i++;
+    document.write(text);
+  }
